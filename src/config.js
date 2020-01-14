ea287e7b7c1cbb97552217912599e9070b274555
Kevin Skinner <nivek.skinner@gmail.com>
2020-01-13 19:46:34 -0800

    commiting before switching branches

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '$2a$10$R4WvUYI3Mq.0poMQMcbXeiLqaDDoYjczbLhv8.2mWDl0bTtqM88G',
 }
